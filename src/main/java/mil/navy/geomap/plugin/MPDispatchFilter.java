b4537186a9a8b44dd92cc3403d8b7f72cfa7117a
dmcintosh <dmcintosh@forwardslope.com>
2016-08-30 09:33:06 -0700

    Ticket #DCGSNSAFE-7709 Add MPSDisplatchFilter and remove test example classes

---
+++ src/main/java/mil/navy/geomap/plugin/MPDispatchFilter.java
@@ -0,0 +1,30 @@
+package mil.navy.geomap.plugin;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.geoserver.platform.AdvancedDispatchFilter;
+
+public class MPDispatchFilter extends AdvancedDispatchFilter {
+	
+	@Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        if (request instanceof HttpServletRequest) {
+        	String path = ((HttpServletRequest) request).getPathInfo();
+        	if (path.indexOf("/mps") == 0) { // if this is an MPS request, bypass GeoServer filtering
+                chain.doFilter(request, response);
+        	} else {
+                super.doFilter(request, response, chain); // perform GeoServer filtering
+        	}
+        } else {
+            super.doFilter(request, response, chain); // perform GeoServer filtering
+        }
+    }
+
+}
--- src/main/java/mil/navy/geomap/plugin/MPSOperation.java
+++
@@ -1,7 +0,0 @@
-package mil.navy.geomap.plugin;
-
-public enum MPSOperation {
-	GET_ROLE,
-	GET_BOOKMARKS,
-	LOAD_FEATURES;
-}
